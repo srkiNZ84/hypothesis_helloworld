46d1b44074a9de701f0608d5bf26726feec992f8
Serge Dukic <srdan.dukic@thewarehouse.co.nz>
2019-03-04 22:33:29 +1300

    Add length check and double reverse check
    Verify that the length matches and also that the when the string is
    reversed twice it then returns the same value.
    
    Issue(s): None

--- test_wordmultiplier.py
+++ test_wordmultiplier.py
@@ -1,11 +1,23 @@
 from wordmultiplier import multiplyWords
 from hypothesis import given, reject
 from hypothesis.strategies import integers, text
+from string import ascii_lowercase
 
-@given(text(max_size=5), integers(min_value=0, max_value=10))
+VALID_WORDS= ascii_lowercase + "_-."
+
+@given(text(alphabet=VALID_WORDS, max_size=10), integers(min_value=0, max_value=10))
 def test_combinations(word, number):
     try:
-        multiplyWords(word, number)
+        returnString = multiplyWords(word, number)
+        assert len(returnString) == len(word * number)
+    except ValueError:
+        reject()
+
+@given(text(alphabet=VALID_WORDS, max_size=10), integers(min_value=0, max_value=10))
+def test_reverse(word, number):
+    try:
+        returnString = multiplyWords(word, number)
+        assert returnString[::-1][::-1] == returnString
     except ValueError:
         reject()
 
